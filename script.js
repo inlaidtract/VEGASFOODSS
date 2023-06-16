845b370c1041d08decc1b119cad57862574233d5
vany <terantuck@gmail.com>
2023-06-16 16:55:01 +0700

    dekstop scale

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-//     if (screenWidth >= 768) {
-//         var warning = document.querySelector('.warning');
-//         warning.style.display = 'block';
-//     }
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (screenWidth >= 768) {
+        var warning = document.querySelector('.warning');
+        warning.style.display = 'block';
+    }
+});
